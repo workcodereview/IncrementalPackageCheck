ed6e617f88f0679388fa86c6d3faacd6d63139eb
zhangyin <zhangyin@kingsoft.com>
2020-10-14 11:40:36 +0800

    添加时间戳 方便版本包于工单建立联系

--- IncrementalPackageCheck/update_single_number.py
+++ IncrementalPackageCheck/update_single_number.py
@@ -257,8 +257,7 @@ if __name__ == '__main__':
                 print('[Test]当前正在分析第' + str(count) + '个提交单,单号为:' + single_number)
                 # 获取当前单的所有信息 文件对应bundle 或者 没有找到bundle的文件列表
                 result_total = analy_single_number(trunk_path_to_bundle, txpublish_path_to_bundle, hotfix_path_to_bundle, root_info)
-                if single_number == 'JX3M-103226':
-                    print(str(result_total))
+
                 count = count + 1
 
                 # 存储此单的描述 经办人 创建时间
